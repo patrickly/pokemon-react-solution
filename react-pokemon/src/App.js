6c2352a8659bd48091fef6585dc095535f0feef0
Patrick Ly <patrickkimly@gmail.com>
2020-01-22 15:46:56 -0800

    Starter files

---
+++ react-pokemon/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          https://pokeapi.co/api/v2/pokemon/?limit=9
+    </div>
+  );
+}
+
+export default App;
