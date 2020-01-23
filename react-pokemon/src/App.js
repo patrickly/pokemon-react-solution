91869ae17c71438227cd8d36974938ffb126eab8
Patrick Ly <patrickkimly@gmail.com>
2020-01-22 16:31:10 -0800

    Added react-router-dom and axios. Can perform a get request for pokemons

--- react-pokemon/src/App.js
+++ react-pokemon/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [pokemonList, setPokemonList] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+        const pokemon =  await axios ({
+          method: 'GET',
+          url: 'https://pokeapi.co/api/v2/pokemon/?limit=9',
+        });
+      setPokemonList(pokemon.data.results);
+      console.log('pokemon' ,pokemon.data)
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-          https://pokeapi.co/api/v2/pokemon/?limit=9
+      {pokemonList && pokemonList.length}
+      df
     </div>
   );
 }
