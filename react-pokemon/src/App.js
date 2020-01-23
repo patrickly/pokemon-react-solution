fa7f78bf889fb4bd2a7ebcf1382717ef7e2a7246
Patrick Ly <patrickkimly@gmail.com>
2020-01-22 16:40:27 -0800

    Added the fetch() api as an alternative to axios

--- react-pokemon/src/App.js
+++ react-pokemon/src/App.js
@@ -13,8 +13,17 @@ const App = () => {
           method: 'GET',
           url: 'https://pokeapi.co/api/v2/pokemon/?limit=9',
         });
-      setPokemonList(pokemon.data.results);
-      console.log('pokemon' ,pokemon.data)
+        setPokemonList(pokemon.data.results);
+        console.log('pokemon', pokemon)
+        console.log('pokemon.data.results', pokemon.data.results)
+/* 
+      const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=9')
+      const pokeJson =  await pokemon.json();
+      console.log('pokemon' ,pokemon)
+      console.log('pokeJson' ,pokeJson.results)
+      setPokemonList(pokeJson.results);
+ */
+
     }
 
     fetchData();
