7076eb2a37251fd53d92a0368a27ecf72005caad
Patrick Ly <patrickkimly@gmail.com>
2020-01-22 18:18:33 -0800

    Formatted Pokemon.js

--- react-pokemon/src/Pokemon.js
+++ react-pokemon/src/Pokemon.js
@@ -4,48 +4,48 @@ import { Link } from 'react-router-dom';
 
 import './App.css';
 
-const Pokemon = ({match}) => {
+const Pokemon = ({ match }) => {
 
-  console.log('match:', match);
-  console.log('match.params.id:', match.params.id);
-  const [name, setName] = useState('');
-  const [picture, setPicture] = useState('');
+    console.log('match:', match);
+    console.log('match.params.id:', match.params.id);
+    const [name, setName] = useState('');
+    const [picture, setPicture] = useState('');
 
-  useEffect(() => {
-    async function fetchData() {
-        const pokemon =  await axios ({
-          method: 'GET',
-          url: `https://pokeapi.co/api/v2/pokemon/${match.params.id}`,
-        });
-        console.log('33pokemon', pokemon)
-        console.log('33pokemon.data', pokemon.data)
-        console.log('33pokemon.data.name', pokemon.data.name)
-        console.log('33pokemon.data.sprites', pokemon.data.sprites)
-        console.log('33pokemon.data.sprites.front_default', pokemon.data.sprites.front_default)
-        setName(pokemon.data.name);
-        setPicture(pokemon.data.sprites.front_default);
+    useEffect(() => {
+        async function fetchData() {
+            const pokemon = await axios({
+                method: 'GET',
+                url: `https://pokeapi.co/api/v2/pokemon/${match.params.id}`,
+            });
+            console.log('33pokemon', pokemon)
+            console.log('33pokemon.data', pokemon.data)
+            console.log('33pokemon.data.name', pokemon.data.name)
+            console.log('33pokemon.data.sprites', pokemon.data.sprites)
+            console.log('33pokemon.data.sprites.front_default', pokemon.data.sprites.front_default)
+            setName(pokemon.data.name);
+            setPicture(pokemon.data.sprites.front_default);
 
-    }
+        }
 
-    fetchData();
-  }, [])
+        fetchData();
+    }, [])
 
-  return (
-    <>
-    <div className="pokemonID">
-        {/* {match.params.id} */}
-        <div className="name">
-            {name}
-        </div>
-        <img src= {picture} alt={name}/>
-    </div>
-    <div>
-             <Link to="/" className="nav-link">
-                                    home
+    return (
+        <>
+            <div className="pokemonID">
+                {/* {match.params.id} */}
+                <div className="name">
+                    {name}
+                </div>
+                <img src={picture} alt={name} />
+            </div>
+            <div>
+                <Link to="/" className="nav-link">
+                    home
             </Link>
-    </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default Pokemon;
